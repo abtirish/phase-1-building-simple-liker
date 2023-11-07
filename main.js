a3a5cd4a19fe3c40cd0a78af23d940e6535e5505
abtirish <abtirish@gmail.com>
2023-11-07 16:00:07 -0700

    finish phase 1 building simple liker

--- main.js
+++ main.js
@@ -4,6 +4,55 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// Add a hidden Class to the DOM to hide the Error
+let modal = document.querySelector('#modal');
+modal.classList.add('hidden');
+
+// Listen for a click on the heart button
+document.querySelector('.like-glyph').addEventListener('click', () => {
+mimicServerCall()
+  .then((response) => {
+    // Define where the heart is on the DOM 
+  let heart = document.querySelector('.like-glyph');
+
+  // Change the empty heart to a full heart
+  heart.textContent = FULL_HEART
+  // Turn the color of the hear red
+  heart.classList.add("activated-heart")
+  })
+  .catch((error) => {
+    // Define where the Error text is on the DOM
+  let removeHidden = document.querySelector('#modal');
+
+  // Remove the hidden class to show the error
+  removeHidden.classList.remove('hidden');
+
+  // Add the hidden class back in to hide the error after 3 seconds
+  setTimeout(() => {
+    removeHidden.classList.add('hidden');
+  }, 3000);
+  })
+})
+
+
+/*
+function isRandomFailure() {
+let removeHidden = document.querySelector('#modal');
+removeHidden.classList.remove('hidden');
+}
+isRandomFailure()
+
+function isNotFailure() {
+  let heart = document.querySelector('.like-glyph');
+
+  heart.textContent = FULL_HEART
+  heart.classList.add("activated-heart")
+
+}
+
+//isNotFailure()
+*/
+
 
 
 
